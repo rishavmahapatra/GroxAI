56d1f97aceade54e92631dc41fae620c8df0b1a1
rishavmahapatra <rishavmahapatra24@gmail.com>
2026-07-27 01:58:20 +0530

    interview sessions added

--- src/components/config.jsx
+++ src/components/config.jsx
@@ -1,3 +1,2 @@
-// export const url = "https://grox-ai-backend.onrender.com";
-// export const url = "https://grox-ai-backend.vercel.app";
-export const url = "http://localhost:8000";
+export const url = "https://grox-ai-backend.vercel.app";
+// export const url = "http://localhost:8000";
